090ae2b1af022bee82b56984bb885ed5691a7540
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-06-13 16:06:33 +0530

    jsonplacholder API data fetched

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import { robots } from './robots';
+//import { robots } from './robots';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css'
+import Scroll from './Scroll' 
 
 
 class App extends Component  {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     }
   }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then(response =>  response.json())
+    .then(users => this.setState({ robots : users }) 
+  );
+}
 
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value })
@@ -24,14 +31,22 @@ class App extends Component  {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
+
+    if(this.state.robots.length === 0){
+      return <h1>Loading</h1>
+    } else{
+      return (
+        <div className="tc">
+          <h1 className='f2'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList robots={filteredRobots}/>
+           </Scroll>
+        </div>
+      );
+    }
     
-    return (
-      <div className="tc">
-        <h1 className='f2'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-         <CardList robots={filteredRobots}/>
-      </div>
-    );
+
 
   }
  
